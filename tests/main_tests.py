7dba2989ce2ddf5ed041b1a257c568b5ac278dda
Gerben Oolbekkink <g.j.w.oolbekkink@student.tudelft.nl>
2014-06-10 16:17:15 +0200

    Test is_default_config

---
+++ tests/main_tests.py
@@ -0,0 +1,24 @@
+import unittest, tempfile
+
+import doorman.main
+
+class MainTests(unittest.TestCase):
+
+	def test_is_default_config_true(self):
+		self.yml = tempfile.NamedTemporaryFile()
+		self.yml.write(doorman.main.DEFAULT_CONFIG)
+		self.yml.seek(0)
+		doorman.main.DEFAULT_CONFIG_FILE = self.yml.name
+		self.assertEquals(doorman.main.is_default_config(), True)
+
+	def test_is_default_config_false(self):
+		self.yml = tempfile.NamedTemporaryFile()
+		self.yml.write(doorman.main.DEFAULT_CONFIG)
+		self.yml.write("test")
+		self.yml.seek(0)
+		doorman.main.DEFAULT_CONFIG_FILE = self.yml.name
+		self.assertEquals(doorman.main.is_default_config(), False)
+
+
+
+
